8078b271942a508ea0308675f55db201e164595b
sadwhalenoises <carlton.shinall@gmail.com>
2022-07-24 15:59:20 -0500

    "Finishing Model"

--- src/main/java/com/example/Hangman.java
+++ src/main/java/com/example/Hangman.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.*;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.geometry.*;
 
@@ -18,6 +21,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * JavaFX App
@@ -26,12 +33,13 @@ public class Hangman extends Application {
 
     private static Scene scene;
     private static Label titleLabel;
-    private static Label testLabel;
+    private static Label userPrompt;
+    public static Label[] stackLabel;
+    public static TextField userText;
 
     @Override
     public void start(Stage stage) throws IOException {
         titleLabel = new Label("Hangman");
-        testLabel = new Label("test");
         titleLabel.setId("title");
         stage.setTitle("Test Hangman Title");
 
@@ -41,10 +49,11 @@ public class Hangman extends Application {
         BorderPane.setAlignment(titleLabel, Pos.TOP_CENTER);
         rootNode.setCenter(addFBox());
         rootNode.setBottom(addCharacters());
+        rootNode.setLeft(userText());
 
 
         File file = new File("src/main/java/com/example/css/styles.css");
-        Scene HangmanScene = new Scene(rootNode, 800, 800);
+        Scene HangmanScene = new Scene(rootNode, 800, 600);
         HangmanScene.getStylesheets().add(file.toURI().toString());
         stage.setScene(HangmanScene);
         stage.show();
@@ -60,6 +69,7 @@ public class Hangman extends Application {
             Image HangmanImg = new Image(file.toURI().toString());
             ImageView baseHangmanImage = new ImageView(HangmanImg);
             fpane.getChildren().add(baseHangmanImage);
+            FlowPane.setMargin(baseHangmanImage, new Insets(0,140,0,0));
             }else{
                 System.out.println("file doesn't exist");
             }
@@ -73,42 +83,77 @@ public class Hangman extends Application {
     }
 
     public FlowPane addCharacters(){
-        String str = "test";
-        Label[] labels = new Label[str.length()];
+
+        String str = Letters.randoLetters();
+        stackLabel = new Label[str.length()];
         FlowPane charBorders = new FlowPane(10, 10);
+        StackPane[] stack = new StackPane[str.length()];
 
         for(int i = 0; i <=str.length()-1; i++){
-            labels[i] = new Label("t");
-            labels[i].getStyleClass().add("letters");
-            charBorders.getChildren().add(labels[i]);
-            FlowPane.setMargin(labels[i], new Insets(2,2,250,2));
+            stackLabel[i] = new Label(Character.toString(str.charAt(i)));
+            stackLabel[i].setId("visible-text");
+            stack[i] = new StackPane();
+            stack[i].getChildren().addAll(new Rectangle(50,50,Color.BLACK), stackLabel[i]);
+            FlowPane.setMargin(stack[i], new Insets(50,2,250,2));
+            charBorders.getChildren().add(stack[i]);
         }
+
+        
         charBorders.setAlignment(Pos.CENTER);
 
         return charBorders;
     }
 
+    public FlowPane userText(){
+        FlowPane user = new FlowPane();
+        user.setVgap(5);
+        user.setOrientation(Orientation.VERTICAL);
+        userText = new TextField();
+        userText.setMaxWidth(30);
+        userPrompt = new Label("Enter a letter to guess");
+        user.getChildren().addAll(userPrompt, userText);
+        FlowPane.setMargin(userText, new Insets(5,0,0,20));
+        FlowPane.setMargin(userPrompt, new Insets(5,0,0,20));
 
-    public enum Element {
-        H("Hydrogen"),
-        HE("Helium"),
-        // ...
-        NE("Neon");
+        return user;
+
+
+    }
+
+
+    public enum Letters {
+        JAVA,
+        PYTHON,
+        MAVEN,
+        MAIL,
+        RUBY,
+        GAMES,
+        SHAGGY,
+        GACHA,
+        LEAGUE,
+        MOTHER,
+        FATHER,
+        SISTER,
+        BROTHER,
+        SOFTWARE,
+        GAMBLE;
+
     
-        public final String label;
-    
-        private Element(String label) {
-            this.label = label;
+        private static final List<Letters> LETTERS =
+            Collections.unmodifiableList(Arrays.asList(values()));
+        private static final int SIZE = LETTERS.size();
+        private static final Random RANDOM = new Random();
+
+        public static String randoLetters() {
+            return LETTERS.get(RANDOM.nextInt(SIZE)).toString();
         }
-        
-        
+            
     }
 
     
 
     public static void main(String[] args) {
         launch();
-        System.out.println(Element.HE.label);
     }
 
 }
